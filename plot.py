412cc43d1d6a65a03990ded18445d72c48488595
edwardwang4340 <eddywang530@gmail.com>
2023-12-26 20:29:51 -0500

    added model plot and loss curve plot

---
+++ plot.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import tensorflow as tf
+from matplotlib import pyplot as plt
+
+def plot_the_model(trained_weight, trained_bias, feature, label):
+  plt.xlabel("feature")
+  plt.ylabel("label")
+
+  plt.scatter(feature, label)
+
+  x0 = 0
+  y0 = trained_bias
+  x1 = feature[-1]
+  y1 = trained_bias + (trained_weight * x1)
+  plt.plot([x0, x1], [y0, y1], c='r')
+
+  plt.show()
+
+def plot_the_loss_curve(epochs, rmse):
+  plt.figure()
+  plt.xlabel("Epoch")
+  plt.ylabel("Root Mean Squared Error")
+
+  plt.plot(epochs, rmse, label="Loss")
+  plt.legend()
+  plt.ylim([rmse.min()*0.97, rmse.max()])
+  plt.show()
